c7a6cf1754645695ab64fcd9f45a617f47e8f1fe
Crisvo <mariancristianvoicu+github@gmail.com>
2022-07-27 20:09:37 +0300

    initial commit

---
+++ src/main/java/ro/backend/DbBackendApplication.java
@@ -0,0 +1,13 @@
+package ro.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/backend/DbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
